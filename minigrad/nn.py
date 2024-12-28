02b61ffbf476be300ea45b30aec8c74f0c2f462b
NembotJules <nembotjulesgithub@gmail.com>
2024-12-28 02:04:22 +0100

    Builded the Layer Class...

--- minigrad/nn.py
+++ minigrad/nn.py
@@ -23,3 +23,15 @@ class Neuron(Module):
     
     def parameters(self): 
         return self.w + [self.b]
+    
+    
+class Layer(Module): 
+    def __init__(self, nin: int, nout: int, activation: callable = None):
+        self.neurons = [Neuron(nin, activation) for _ in range(nout)]
+
+    def __call__(self, x): 
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return[p for n in self.neurons for p in n.parameters()]
